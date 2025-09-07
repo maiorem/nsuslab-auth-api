11bc666f751a79776f508200763f1d6e4e65d67e
Seyoung Hong <maiorem00@gmail.com>
2025-09-07 13:12:37 +0900

    feat: JPA 모듈 설정

---
+++ modules/jpa/src/main/java/com/nsuslab/jpa/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package com.nsuslab.jpa.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EntityScan({"com.nsuslab"})
+public class JpaConfig {
+}
